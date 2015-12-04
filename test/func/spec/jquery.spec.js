81b23ae81757ba77af4414c9eea082227409cc6c
Ryan Roemer <ryan.roemer@formidablelabs.com>
2015-12-04 12:31:29 -0800

    Reorder jquery asserts to match load order. /cc @exogen

--- test/func/spec/jquery.spec.js
+++ test/func/spec/jquery.spec.js
@@ -22,52 +22,10 @@ describe("error", function () {
         expect(text).to.equal("PASS");
       })
 
-      .getText(".e2e-google-1-11-3-2-error").then(function (text) {
+      .getText(".e2e-jquery-1-11-3-1-error").then(function (text) {
         expect(text).to.equal("");
       })
-      .getText(".e2e-google-1-11-3-2-result").then(function (text) {
-        expect(text).to.equal("PASS");
-      })
-
-      .getText(".e2e-google-1-11-1-1-error").then(function (text) {
-        expect(text).to.equal("");
-      })
-      .getText(".e2e-google-1-11-1-1-result").then(function (text) {
-        expect(text).to.equal("PASS");
-      })
-
-      .getText(".e2e-google-1-7-2-1-error").then(function (text) {
-        expect(text).to.equal("");
-      })
-      .getText(".e2e-google-1-7-2-1-result").then(function (text) {
-        expect(text).to.equal("PASS");
-      })
-
-      .getText(".e2e-jquery-1-11-3-2-error").then(function (text) {
-        expect(text).to.equal("");
-      })
-      .getText(".e2e-jquery-1-11-3-2-result").then(function (text) {
-        expect(text).to.equal("PASS");
-      })
-
-      .getText(".e2e-jquery-1-11-2-1-error").then(function (text) {
-        expect(text).to.equal("");
-      })
-      .getText(".e2e-jquery-1-11-2-1-result").then(function (text) {
-        expect(text).to.equal("PASS");
-      })
-
-      .getText(".e2e-jquery-1-7-2-1-error").then(function (text) {
-        expect(text).to.equal("");
-      })
-      .getText(".e2e-jquery-1-7-2-1-result").then(function (text) {
-        expect(text).to.equal("PASS");
-      })
-
-      .getText(".e2e-jquery-1-11-1-1-error").then(function (text) {
-        expect(text).to.equal("");
-      })
-      .getText(".e2e-jquery-1-11-1-1-result").then(function (text) {
+      .getText(".e2e-jquery-1-11-3-1-result").then(function (text) {
         expect(text).to.equal("PASS");
       })
 
@@ -78,12 +36,55 @@ describe("error", function () {
         expect(text).to.equal("PASS");
       })
 
-      .getText(".e2e-jquery-1-11-3-1-error").then(function (text) {
+      .getText(".e2e-jquery-1-11-2-1-error").then(function (text) {
         expect(text).to.equal("");
       })
-      .getText(".e2e-jquery-1-11-3-1-result").then(function (text) {
+      .getText(".e2e-jquery-1-11-2-1-result").then(function (text) {
         expect(text).to.equal("PASS");
       })
+
+      .getText(".e2e-google-1-7-2-1-error").then(function (text) {
+        expect(text).to.equal("");
+      })
+      .getText(".e2e-google-1-7-2-1-result").then(function (text) {
+        expect(text).to.equal("PASS");
+      })
+
+      .getText(".e2e-google-1-11-1-1-error").then(function (text) {
+        expect(text).to.equal("");
+      })
+      .getText(".e2e-google-1-11-1-1-result").then(function (text) {
+        expect(text).to.equal("PASS");
+      })
+
+      .getText(".e2e-jquery-1-11-1-1-error").then(function (text) {
+        expect(text).to.equal("");
+      })
+      .getText(".e2e-jquery-1-11-1-1-result").then(function (text) {
+        expect(text).to.equal("PASS");
+      })
+
+      .getText(".e2e-jquery-1-7-2-1-error").then(function (text) {
+        expect(text).to.equal("");
+      })
+      .getText(".e2e-jquery-1-7-2-1-result").then(function (text) {
+        expect(text).to.equal("PASS");
+      })
+
+      .getText(".e2e-google-1-11-3-2-error").then(function (text) {
+        expect(text).to.equal("");
+      })
+      .getText(".e2e-google-1-11-3-2-result").then(function (text) {
+        expect(text).to.equal("PASS");
+      })
+
+      .getText(".e2e-jquery-1-11-3-2-error").then(function (text) {
+        expect(text).to.equal("");
+      })
+      .getText(".e2e-jquery-1-11-3-2-result").then(function (text) {
+        expect(text).to.equal("PASS");
+      })
+
 
       .finally(base.promiseDone(done));
   });
